81470b9901b71285ad8634cb3138c99e9c28927a
demorite <dylan3396@hotmail.fr>
2015-03-16 16:51:50 +0100

    Menage

--- wordpress/wp-content/themes/gibson/content-page.php
+++
@@ -1,8 +0,0 @@
-<?php the_post_thumbnail('featured'); ?>
-<div class="col-md-10 col-md-offset-1">	
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>				
-		<h1 id="post-title"><?php the_title(); ?></h1>
-		<?php the_content(); ?>	
-		<?php wp_link_pages(); ?>		
-	</article>	
-</div>
--- wordpress/wp-content/themes/leatherdiary/js/responzive.js
+++
@@ -1,3 +0,0 @@
-/* Responzive.js v1.0 */
-selectnav('fix-nav');
-selectnav('top-nav');
